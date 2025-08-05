2ffe5c74cad4d8ba0875c2999b9d19b73d78f570
Amir <eyemir.nf@gmail.com>
2025-08-06 01:09:42 +0330

    fix: re-enable Add Movie button after back button is clicked

--- src/scripts/core/backButton.js
+++ src/scripts/core/backButton.js
@@ -1,10 +1,15 @@
 import { App } from "@capacitor/app";
 import { closeModals, isAnyModalOpen } from "../ui/modal";
+import { toggleBtnDisabled } from "../utils/dom";
+import { $ } from "../ui/dom";
 
 export const setupBackButtonHandler = () => {
 	App.addListener("backButton", () => {
 		if (isAnyModalOpen()) {
 			closeModals();
+
+			const addMovieBtn = $(".add-btn");
+			toggleBtnDisabled(addMovieBtn, false);
 			return;
 		}
 
